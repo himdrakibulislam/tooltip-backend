032f6a2b43971b2fcafde02195cab81a66d8c4a4
Rakibul islam <himdrakibulislam@gmail.com>
2024-01-04 22:37:28 +0600

    register-user

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
-const app = express();
-app.use(cors({
+const app = express(); 
+app.use(cors({  
     origin : process.env.CORS_ORIGIN,
     credentitals : true
 }));
