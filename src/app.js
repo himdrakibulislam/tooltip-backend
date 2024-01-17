43263933df666ddc5dc2ae2caf062c382273599a
Rakibul Islam <himdrakibulislam@gmail.com>
2024-01-17 13:50:42 +0600

    course

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ import cors from "cors"
 const app = express(); 
 app.use(cors({  
     origin : process.env.CORS_ORIGIN,
-    credentitals : true
+    credentials : true
 }));
+
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extend:true,limit:"16kb"}));
 app.use(express.static("public"));
@@ -16,6 +17,5 @@ import userRouter from "./routes/user.routes.js";
 
 // routes declaration 
 app.use("/api/v1/users",userRouter);
-
  
 export {app};
--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     try {
        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
        console.log(`\n MONGODB connected !! DB HOST : ${connectionInstance.connection.host} `);
-       
     } catch (error) {
         console.log("MONGODB CONNECTION ERROR",error);
         process.exit(1);
